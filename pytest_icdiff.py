18430692f6a9233fb062352625dd5f58b2b2a6a4
Harry <hjwp2@cantab.net>
2018-08-08 16:47:55 +0100

    nicer preamble message

--- pytest_icdiff.py
+++ pytest_icdiff.py
@@ -4,7 +4,7 @@ import icdiff
 def pytest_assertrepr_compare(config, op, left, right):
     if op != '==':
         return
-    return ['failed'] + list(icdiff.ConsoleDiff(tabsize=2).make_table(
+    return ['equals failed'] + list(icdiff.ConsoleDiff(tabsize=2).make_table(
         pformat(left, width=40).splitlines(),
         pformat(right, width=40).splitlines(),
     ))
